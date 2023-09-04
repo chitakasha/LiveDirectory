e29e0ca2108c355c5744d4859c9de4621318492a
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-09-04 16:41:17 -0700

    Update golden_ratio_autogen.py

--- core/autogen/golden_ratio_autogen.py
+++ core/autogen/golden_ratio_autogen.py
@@ -1,1 +1,43 @@
+import json
+import random
+
+def generate_text(prompts, current_state, settings):
+    """
+    Generates text based on the golden ratio, current state, and inbox prompts.
+
+    Parameters:
+        prompts (dict): The inbox prompts.
+        current_state (dict): The current state of the directory.
+        settings (dict): The settings from `config/settings.json`.
+
+    Returns:
+        str: The generated text.
+    """
+
+    phi = settings['golden_ratio']['phi']
+    iterations = settings['golden_ratio']['iterations']
+    threshold = settings['general']['autogen_threshold']
+
+    # Initialize variables
+    generated_text = ""
+    prompt_list = prompts.get('prompts', [])
+    
+    # Check if there are any prompts
+    if not prompt_list:
+        return "No prompts available."
+
+    # Apply golden ratio logic
+    for i in range(iterations):
+        index = int((phi * i) % len(prompt_list))
+        selected_prompt = prompt_list[index]
+
+        # Apply some logic to generate text based on the selected prompt and current state
+        # For demonstration, we'll just append the selected prompt
+        generated_text += selected_prompt + " "
+
+        # Check if generated text meets some criteria (e.g., length, complexity)
+        if len(generated_text.split()) / iterations >= threshold:
+            break
+
+    return generated_text.strip()
 
