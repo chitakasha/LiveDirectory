930162f1ed0747763de4b3d44e4d1cfa6de74247
Pasha Mango <141678664+pashamango@users.noreply.github.com>
2023-09-04 18:10:59 -0700

    Update test_autogen.py

--- tests/test_autogen.py
+++ tests/test_autogen.py
@@ -1,1 +1,24 @@
+import unittest
+
+from core.autogen import golden_ratio_autogen
+
+
+class TestGoldenRatioAutogen(unittest.TestCase):
+    """Tests for the golden ratio autogeneration module."""
+
+    def test_generate_text(self):
+        """Tests the generate_text() function."""
+        prompts = [
+            "What is the meaning of life?",
+            "How can we achieve world peace?",
+            "What are the principles of quantum mechanics?",
+        ]
+        current_state = {}
+        generated_text = golden_ratio_autogen.generate_text(prompts, current_state)
+        self.assertIsNotNone(generated_text)
+        self.assertGreater(len(generated_text), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
 
